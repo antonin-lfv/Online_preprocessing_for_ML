050d4289440529039e9b725c68bf231dcd1d2b07
Antonin <antoninlefevre45@icloud.com>
2020-06-30 23:13:08 +0200

    Update etude_dataset_toolbox.py

--- etude_dataset_toolbox.py
+++ etude_dataset_toolbox.py
@@ -1,4 +1,4 @@
-""" Etude d'un dataframe """
+""" Study of a dataframe """
 
 import numpy as np
 from collections import Counter
@@ -7,7 +7,7 @@ import time
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-## avoir des renseignement sur une série
+## have information on a series
 
 def column_info(data): # data -> 1 serie
     (a,) = data.shape
@@ -21,7 +21,7 @@ def column_info(data): # data -> 1 serie
     print(data.head(5))
     print('-------------------------')
     time.sleep(1)
-    print(' ● type de données :', type(data))
+    print(' ● data type :', type(data))
     print('-------------------------')
     time.sleep(2)
     if n_data.dtype == float :
@@ -29,7 +29,7 @@ def column_info(data): # data -> 1 serie
         variance = data.std()
         max = data.max()
         min = data.min()
-        print(' ● moyenne :', moyenne)
+        print(' ● average :', moyenne)
         print('-------------------------')
         time.sleep(1)
         print(' ● variance :', variance)
@@ -41,36 +41,53 @@ def column_info(data): # data -> 1 serie
         print(' ● minimum :', min)
         print('-------------------------')
         time.sleep(1)
-    print(' ● valeur les plus présentes:',(Counter(n_data).most_common()[0])[0],':',(Counter(n_data).most_common()[0])[1],'fois',',',(Counter(n_data).most_common()[1])[0],':',(Counter(n_data).most_common()[1])[1],'fois')
+    print(' ● most present value:',(Counter(n_data).most_common()[0])[0],':',(Counter(n_data).most_common()[0])[1],'fois',',',(Counter(n_data).most_common()[1])[0],':',(Counter(n_data).most_common()[1])[1],'fois')
     print('-------------------------')
     time.sleep(1)
-    print(' ● valeurs manquantes:', sum(pd.DataFrame(n_data).isnull().sum(axis=1).tolist()))
+    print(' ● missing values:', sum(pd.DataFrame(n_data).isnull().sum(axis=1).tolist()))
     print('-------------------------')
     time.sleep(1)
-    print(' ● longueur:',n_data.shape[0])
+    print(' ● length:',n_data.shape[0])
     print('-------------------------')
     time.sleep(1)
-    print(' ● nombre de valeurs distinctes non NaN:', abs(len(Counter(n_data))-sum(pd.DataFrame(n_data).isnull().sum(axis=1).tolist())))
+    print(' ● number of different values not NaN:', abs(len(Counter(n_data))-sum(pd.DataFrame(n_data).isnull().sum(axis=1).tolist())))
     print('-------------------------')
     time.sleep(1)
 
 
-## avoir des renseignements sur le dataframe entier
+## have information on the entire dataframe
 
-#dataset de type Dataframe :
+def max_std(dataset): 
+    l = []
+    for nom in dataset.columns :
+        if type(dataset[nom][0]) != object and type(dataset[nom][0]) != str :
+            l.append([dataset[nom].std(),nom])
+    return(max(l))
+
+#dataset with type Dataframe :
 " Numpy -> pandas.Dataframe : pd.DataFrame()"
 
-def dataset_info(dataset):
-    print(' ● taille:', dataset.shape)
+def dataset_info(dataset, nom_col_index): #type(nom_col_index)=str
+    print('')
+    print(' ● size:', dataset.shape)
     print('-------------------------')
-    time.sleep(1)
-    print(' ● types de données:', dataset.dtypes.value_counts())
+    time.sleep(0.5)
+    print(' ● data type:', dataset.dtypes.value_counts())
     print('-------------------------')
-    time.sleep(2)
-    print(' ● valeurs manquantes:', sum(pd.DataFrame(dataset).isnull().sum(axis=1).tolist()))
+    time.sleep(0.5)
+    print(' ● missing values:', sum(pd.DataFrame(dataset).isnull().sum(axis=1).tolist()))
     print('-------------------------')
-    time.sleep(2)
-    print(' ● nombre de valeurs:', dataset.shape[0]*dataset.shape[1])
-    sns.heatmap(dataset.isna(),cbar=False)
-    plt.title('dataset vizualisation')
+    time.sleep(0.5)
+    print(' ● number of values:', dataset.shape[0]*dataset.shape[1])
+    print('-------------------------')
+    time.sleep(0.5)
+    print(' ● maximum variance for the column :', max_var(dataset)[1])
+    print('   with a variance of :',max_var(dataset)[0] )
+    print('-------------------------')
+    if sum(pd.DataFrame(dataset).isnull().sum(axis=1).tolist()) != 0 :
+        sns.heatmap(dataset.isna(),cbar=False)
+        plt.title('brigth color = no data') #if missing data we plot the dataset
+    dataset[[nom_col_index,str(max_var(dataset)[1])]].plot()
+    plt.xlabel(nom_col_index)
+    plt.title('most progressive feature')
     plt.show()
