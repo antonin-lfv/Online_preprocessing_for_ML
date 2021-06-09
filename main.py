307bc738d985ff501a8e606b0fcdf140cb941851
Antonin <antoninlefevre45@icloud.com>
2021-06-09 15:15:38 +0200

    css

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
 import os
+st.set_page_config(layout="wide")
 
 ###### functions ########
 def max_std(dataset):
@@ -18,102 +19,115 @@ def max_std(dataset):
             l.append([dataset[nom].std(),nom])
     return(max(l))
 
+####### html/css config ########
+st.markdown("""
+<style>
+.first_titre {
+    font-size:50px !important;
+    font-weight: bold;
+    box-sizing: border-box;
+    text-align: center;
+    width: 100%;
+    border: solid #CD2727 5px;
+    padding: 5px;
+}
+.intro{
+    text-align: justify;
+}
+.grand_titre {
+    font-size:30px !important;
+    font-weight: bold;
+    text-decoration: underline;
+    text-decoration-color: #2782CD;
+    text-decoration-thickness: 5px;
+}
+.section{
+    font-size:20px !important;
+    text-decoration: underline;
+}
+.caract{
+    font-size:10px !important;
+}
+</style>
+""", unsafe_allow_html=True)
 
-####### Streamlit Config ######
-st.set_page_config(layout="wide", )
-st.title('Preprocessing automatique')
+####### Streamlit home ######
+st.markdown('<p class="first_titre">Preprocessing automatique</p>', unsafe_allow_html=True)
+st.write("##")
+st.markdown('<p class="intro">Bienvenue sur le site de Preprocessing en ligne ! Déposez vos dataset csv et excel et commencez votre analyse dès maintenant ! Cherchez les variables les plus intéressantes pour développer votre modèle, ou simplement pour visualiser vos données.</p>',unsafe_allow_html=True)
 
 
 ###### App #######
 uploaded_file = st.sidebar.file_uploader("Chargez votre dataset",type=['csv','xls'])
 if uploaded_file is not None:
     file_details = {"FileName":uploaded_file.name,"FileType":uploaded_file.type,"FileSize":uploaded_file.size}
-    st.sidebar.success('Fichier chargé')
-    if 'csv' in file_details['FileName']:
-        try :
+    try :
+        if 'csv' in file_details['FileName']:
             data = pd.read_csv(uploaded_file)
             slider_col = st.sidebar.selectbox(
                 'Choisissez une colonne à étudier',
-                ['Choisir']+data.columns.to_list(),
+                ['Choisir'] + data.columns.to_list(),
             )
-            ### section du dataset ###
-            st.write("##")
-            st.write("Aperçu du dataset : ")
-            st.write(data.head(50))
-
-            st.write("##")
-            st.write(' ● size:', data.shape)
-            st.write(' ● data type:', data.dtypes.value_counts())
-            st.write(' ● missing values:', round(
-                sum(pd.DataFrame(data).isnull().sum(axis=1).tolist()) * 100 / (data.shape[0] * data.shape[1]), 2),
-                     ' % (', sum(pd.DataFrame(data).isnull().sum(axis=1).tolist()), ' valeurs manquantes)')
-            st.write(' ● number of values:', data.shape[0] * data.shape[1])
-            ### Section de la colonne ###
-            if slider_col!='Choisir':
-                st.subheader('Colonne étudiée : ',slider_col)
-                ### Données ###
-                n_data = data['{}'.format(slider_col)].to_numpy()
-
-                st.write(' ● données:')
-                st.write(data.head(5))
-                st.write("##")
-                st.write(' ● data type :', type(data))
-                st.write("##")
-                if n_data.dtype == float:
-                    moyenne = data.mean()
-                    variance = data.std()
-                    max = data.max()
-                    min = data.min()
-                    st.write(' ● average :', moyenne)
-                    st.write("##")
-
-                    st.write(' ● variance :', variance)
-                    st.write("##")
-
-                    st.write(' ● maximum :', max)
-                    st.write("##")
-
-                    st.write(' ● minimum :', min)
-                    st.write("##")
-
-                st.write(' ● most present value:', (Counter(n_data).most_common()[0])[0], ':',
-                         (Counter(n_data).most_common()[0])[1], 'fois', ',', (Counter(n_data).most_common()[1])[0], ':',
-                         (Counter(n_data).most_common()[1])[1], 'fois')
-                st.write("##")
-
-                st.write(' ● missing values:', sum(pd.DataFrame(n_data).isnull().sum(axis=1).tolist()))
-                st.write("##")
-
-                st.write(' ● length:', n_data.shape[0])
-                st.write("##")
-
-                st.write(' ● number of different values not NaN:',
-                         abs(len(Counter(n_data)) - sum(pd.DataFrame(n_data).isnull().sum(axis=1).tolist())))
-                st.write("##")
-                ### Fin section données
-            ### Fin section colonne
-
-        except :
-            st.sidebar.error('Erreur de chargement')
-    else :
-        try :
+        else:
             data = pd.read_excel(uploaded_file)
             slider_col = st.sidebar.selectbox(
                 'Choisissez une colonne à étudier',
-                data.columns.to_list(),
+                ['Choisir'] + data.columns.to_list(),
             )
+
+        st.sidebar.success('Fichier chargé')
+        ### section du dataset ###
+        st.write("##")
+        st.markdown('<p class="grand_titre">Analyse du dataset</p>', unsafe_allow_html=True)
+        st.write("##")
+        st.markdown('<p class="section">Aperçu</p>', unsafe_allow_html=True)
+        st.write(data.head(50))
+        st.write("##")
+
+        st.markdown('<p class="section">Caractéristiques</p>', unsafe_allow_html=True)
+        st.write(' - Taille:', data.shape)
+        st.write(' - Nombre de valeurs:', data.shape[0] * data.shape[1])
+        st.write(' - Type des colonnes:', data.dtypes.value_counts())
+        st.write(' - Pourcentage de valeurs manquantes:', round(
+            sum(pd.DataFrame(data).isnull().sum(axis=1).tolist()) * 100 / (data.shape[0] * data.shape[1]), 2),
+                 ' % (', sum(pd.DataFrame(data).isnull().sum(axis=1).tolist()), ' valeurs manquantes)')
+
+        ### Section de la colonne ###
+        if slider_col != 'Choisir':
+            st.write('##')
+            st.markdown('<p class="grand_titre">Analyse de la colonne '+slider_col+'</p>', unsafe_allow_html=True)
+            ### Données ###
+            data_col = data[slider_col].copy()
+            n_data = data[slider_col].to_numpy()
+
+            st.write(' ● aperçu des données:')
+            st.write(data_col.head(5))
+            st.write(' ● type de la colonne :', type(data_col))
+            if n_data.dtype == float:
+                moyenne = data_col.mean()
+                variance = data_col.std()
+                max = data_col.max()
+                min = data_col.min()
+                st.write(' ● Moyenne :', moyenne)
+
+                st.write(' ● Variance :', variance)
+
+                st.write(' ● Maximum :', max)
+
+                st.write(' ● Minimum :', min)
+
+            st.write(' ● Valeurs les plus présentes:', (Counter(n_data).most_common()[0])[0], '->',
+                     (Counter(n_data).most_common()[0])[1], 'fois', ', ', (Counter(n_data).most_common()[1])[0], '->',
+                     (Counter(n_data).most_common()[1])[1], 'fois')
+
+            st.write(' ● Nombre de valeurs manquantes:', sum(pd.DataFrame(n_data).isnull().sum(axis=1).tolist()))
+
+            st.write(' ● Longueur:', n_data.shape[0])
+
+            st.write(' ● Nombre de valeurs différentes non NaN:',
+                     abs(len(Counter(n_data)) - sum(pd.DataFrame(n_data).isnull().sum(axis=1).tolist())))
             st.write("##")
-            st.write("Aperçu du dataset : ")
-            st.write(data.head(50))
-
-            st.write("##")
-            st.write(' ● size:', data.shape)
-            st.write(' ● data type:', data.dtypes.value_counts())
-            st.write(' ● missing values:', round(
-                sum(pd.DataFrame(data).isnull().sum(axis=1).tolist()) * 100 / (data.shape[0] * data.shape[1]), 2),
-                     ' % (', sum(pd.DataFrame(data).isnull().sum(axis=1).tolist()), ' valeurs manquantes)')
-            st.write(' ● number of values:', data.shape[0] * data.shape[1])
-        except :
-            st.sidebar.error('Erreur de chargement')
-
-
+            ### Fin section données
+        ### Fin section colonne
+    except:
+        st.sidebar.error('Erreur de chargement')
