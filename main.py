08d7eabd1451855f0fc49f7d5e1b73e449e7f715
Antonin <antoninlefevre45@icloud.com>
2021-06-24 16:42:12 +0200

    KNN

--- main.py
+++ main.py
@@ -705,9 +705,10 @@ def page1_ML(state):
                                     if "" not in features :
                                         x = np.array(features).reshape(1, len(features))
                                         state.p = (model.predict(x))
-                                        st.write("##")
-                                        st.markdown('<p class="section">Résultats</p>', unsafe_allow_html=True)
-                                        st.success("Prédiction de la target "+state.target+" : "+str(state.p))
+                                        with col1:
+                                            st.write("##")
+                                            st.markdown('<p class="section">Résultats</p>', unsafe_allow_html=True)
+                                            st.success("Prédiction de la target "+state.target+" : "+str(state.p))
                                 except :
                                     with col1:
                                         st.write("##")
