f4c4ab98cafe1f9ae85543a8748d5518e19c1f81
Antonin <antoninlefevre45@icloud.com>
2021-11-09 23:30:13 +0100

    objects detection

--- main.py
+++ main.py
@@ -1213,60 +1213,62 @@ elif choix_page == "Deep Learning":
 
         if photo_to_detect :
             if placeholder_button.button("Lancer la détection"):
-                image_np = load_image_into_numpy_array(photo_to_detect)
-                detector = get_model_detection()
-                detector_output = detector(image_np)
+                with st.spinner('Processus en cours ... '):
+                    image_np = load_image_into_numpy_array(photo_to_detect)
+                    detector = get_model_detection()
+                    detector_output = detector(image_np)
 
-                COCO17_HUMAN_POSE_KEYPOINTS = [(0, 1),
-                                               (0, 2),
-                                               (1, 3),
-                                               (2, 4),
-                                               (0, 5),
-                                               (0, 6),
-                                               (5, 7),
-                                               (7, 9),
-                                               (6, 8),
-                                               (8, 10),
-                                               (5, 6),
-                                               (5, 11),
-                                               (6, 12),
-                                               (11, 12),
-                                               (11, 13),
-                                               (13, 15),
-                                               (12, 14),
-                                               (14, 16)]
+                    COCO17_HUMAN_POSE_KEYPOINTS = [(0, 1),
+                                                   (0, 2),
+                                                   (1, 3),
+                                                   (2, 4),
+                                                   (0, 5),
+                                                   (0, 6),
+                                                   (5, 7),
+                                                   (7, 9),
+                                                   (6, 8),
+                                                   (8, 10),
+                                                   (5, 6),
+                                                   (5, 11),
+                                                   (6, 12),
+                                                   (11, 12),
+                                                   (11, 13),
+                                                   (13, 15),
+                                                   (12, 14),
+                                                   (14, 16)]
 
-                PATH_TO_LABELS = 'mscoco_label_map.pbtxt'
-                category_index = create_category_index_from_labelmap(PATH_TO_LABELS, use_display_name=True)
+                    PATH_TO_LABELS = 'mscoco_label_map.pbtxt'
+                    category_index = create_category_index_from_labelmap(PATH_TO_LABELS, use_display_name=True)
 
-                label_id_offset = 0
-                image_np_with_detections = image_np.copy()
+                    label_id_offset = 0
+                    image_np_with_detections = image_np.copy()
 
-                result = {key: value.numpy() for key, value in detector_output.items()}
+                    result = {key: value.numpy() for key, value in detector_output.items()}
 
-                # Use keypoints if available in detections
-                keypoints, keypoint_scores = None, None
-                if 'detection_keypoints' in result:
-                    keypoints = result['detection_keypoints'][0]
-                    keypoint_scores = result['detection_keypoint_scores'][0]
+                    # Use keypoints if available in detections
+                    keypoints, keypoint_scores = None, None
+                    if 'detection_keypoints' in result:
+                        keypoints = result['detection_keypoints'][0]
+                        keypoint_scores = result['detection_keypoint_scores'][0]
 
-                viz_utils.visualize_boxes_and_labels_on_image_array(
-                    image_np_with_detections[0],
-                    result['detection_boxes'][0],
-                    (result['detection_classes'][0] + label_id_offset).astype(int),
-                    result['detection_scores'][0],
-                    category_index,
-                    use_normalized_coordinates=True,
-                    max_boxes_to_draw=200,
-                    min_score_thresh=.30,
-                    agnostic_mode=False,
-                    keypoints=keypoints, )
+                    viz_utils.visualize_boxes_and_labels_on_image_array(
+                        image_np_with_detections[0],
+                        result['detection_boxes'][0],
+                        (result['detection_classes'][0] + label_id_offset).astype(int),
+                        result['detection_scores'][0],
+                        category_index,
+                        use_normalized_coordinates=True,
+                        max_boxes_to_draw=200,
+                        min_score_thresh=.30,
+                        agnostic_mode=False,
+                        keypoints=keypoints, )
 
-                plt.figure(figsize=(24, 32))
-                plt.imshow(image_np_with_detections[0])
-                plt.savefig("images/tensorflow_images/objects_detector/output.png")
-                st.write("##")
-                placeholder_image.image("images/tensorflow_images/objects_detector/output.png")
+                    plt.figure(figsize=(24, 32))
+                    plt.imshow(image_np_with_detections[0])
+                    plt.savefig("images/tensorflow_images/objects_detector/output.png")
+                    st.write("##")
+                    placeholder_image.empty()
+                    placeholder_image.image("images/tensorflow_images/objects_detector/output.png")
 
 
     elif choix_page_dl == "Génération de citations":
