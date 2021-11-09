13b67610b5671279aac5ee99c598e30c1430a8b1
Antonin <antoninlefevre45@icloud.com>
2021-11-09 17:28:45 +0100

    objects detection

--- main.py
+++ main.py
@@ -171,7 +171,7 @@ def get_model_transfert_learning():
 
 @st.cache
 def get_model_detection():
-    return hub.load("faster_rcnn_inception_resnet_v2_640x640_1")
+    return hub.load("models/faster_rcnn_inception_resnet_v2_640x640_1")
 
 ##################################
 ####### Code streamlit app #######
