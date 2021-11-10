241b333d75afa2675289daab6da43622c4f68ccd
Antonin <antoninlefevre45@icloud.com>
2021-11-10 14:24:55 +0100

    objects detection

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ from label_map_util2 import *
 
 ####### html/css config ########
 st.set_page_config(layout="wide")
-st.sidebar.image('logo')
+st.sidebar.image('logo.png')
 st.markdown("""
 <style>
 .first_titre {
