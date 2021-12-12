3b7ec850afa10ce1cec5119bb05f50a0198ac7bf
Antonin <antoninlefevre45@icloud.com>
2021-12-12 17:19:12 +0100

    Matrices corr

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ from umap import UMAP
 from scipy.spatial import distance
 from sklearn.cluster import KMeans
 from sklearn.svm import SVC
+from utils import *
 
 ####### html/css config ########
 st.set_page_config(layout="wide")
@@ -83,9 +84,7 @@ st.markdown("""
 st.sidebar.image("logo/NAP_logo.png", use_column_width=True)
 st.sidebar.write("##")
 
-
-###### Fonctions #######
-
+###### Load data #######
 def load_data():
     try:
         if 'csv' in st.session_state.file_details['FileName']:
@@ -100,40 +99,6 @@ def load_data():
                 st.session_state.data = pd.read_excel(uploaded_file)
     except:
         pass
-
-
-def max_std(dataset):  # colonne de maximum de variance
-    l = []
-    for nom in dataset.columns:
-        if type(dataset[nom][0]) != object and type(dataset[nom][0]) != str:
-            l.append([dataset[nom].std(), nom])
-    return (max(l))
-
-
-def col_numeric(df):  # retourne les colonnes numériques d'un dataframe
-    return df.select_dtypes(include=np.number).columns.tolist()
-
-
-def col_temporal(df):  # retourne les colonnes temporelles d'un dataframe
-    return df.select_dtypes(include=np.datetime64).columns.tolist()
-
-
-def clean_data(x):  # enlever les symboles d'une colonne
-    if isinstance(x, str):
-        return (x.replace('$', '').replace(',', '').replace('€', '').replace('£', ''))
-    return (x)
-
-
-def distance_e(x, y):  # distance entre 2 points du plan cartésien
-    return distance.euclidean([x[0], x[1]], [y[0], y[1]])
-
-
-def max_dist(donnee_apres_pca, df, voisins):  # pour knn, retourne la distance du voisins le plus loin
-    distances = []
-    for i in range(len(df)):
-        distances.append(distance_e(donnee_apres_pca, [df['x'].iloc[i], df['y'].iloc[i]]))
-    distances.sort()
-    return distances[voisins - 1]
 
 
 ##################################
---
+++ utils.py
@@ -0,0 +1,32 @@
+def max_std(dataset):  # colonne de maximum de variance
+    l = []
+    for nom in dataset.columns:
+        if type(dataset[nom][0]) != object and type(dataset[nom][0]) != str:
+            l.append([dataset[nom].std(), nom])
+    return (max(l))
+
+
+def col_numeric(df):  # retourne les colonnes numériques d'un dataframe
+    return df.select_dtypes(include=np.number).columns.tolist()
+
+
+def col_temporal(df):  # retourne les colonnes temporelles d'un dataframe
+    return df.select_dtypes(include=np.datetime64).columns.tolist()
+
+
+def clean_data(x):  # enlever les symboles d'une colonne
+    if isinstance(x, str):
+        return (x.replace('$', '').replace(',', '').replace('€', '').replace('£', ''))
+    return (x)
+
+
+def distance_e(x, y):  # distance entre 2 points du plan cartésien
+    return distance.euclidean([x[0], x[1]], [y[0], y[1]])
+
+
+def max_dist(donnee_apres_pca, df, voisins):  # pour knn, retourne la distance du voisins le plus loin
+    distances = []
+    for i in range(len(df)):
+        distances.append(distance_e(donnee_apres_pca, [df['x'].iloc[i], df['y'].iloc[i]]))
+    distances.sort()
+    return distances[voisins - 1]
