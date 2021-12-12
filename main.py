7c1df0fec0f985962d4df2b3ce01b6876e531cb9
Antonin <antoninlefevre45@icloud.com>
2021-12-12 22:33:14 +0100

    Matrices corr

--- main.py
+++ main.py
@@ -146,7 +146,6 @@ if choix_page == "Accueil":
             '<p class="intro">Bienvenue sur la <b>no-code AI platform</b> ! Déposez vos datasets csv ou excel ou choisissez en un parmi ceux proposés et commencez votre analyse dès maintenant ! Cherchez les variables les plus intéressantes, visualisez vos données, et créez vos modèles de Machine Learning en toute simplicité.' +
             ' Si vous choisissez de travailler avec votre dataset et que vous voulez effectuez des modifications sur celui-ci, il faudra le télécharger une fois les modifications faites pour pouvoir l\'utiliser sur les autres pages. </p>',
             unsafe_allow_html=True)
-        st.write("##")
         st.markdown(
             '<p class="intro">Un tutoriel sur l\'utilisation de ce site est disponible sur le repo Github. En cas de bug ou d\'erreur veuillez m\'en informer par mail ou sur Discord.</p>',
             unsafe_allow_html=True)
@@ -155,7 +154,6 @@ if choix_page == "Accueil":
             unsafe_allow_html=True)
     c1, _, _, _, _, _ = st.columns(6)
     with c1:
-        st.write("##")
         st.subheader("Liens")
         st.write("• [Mon profil GitHub](https://github.com/antonin-lfv/Online_preprocessing_for_ML/blob/master/README.md)")
         st.write("• [Mon site](https://antonin-lfv.github.io)")
