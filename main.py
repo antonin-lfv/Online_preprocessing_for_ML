9a0056847146021cdce25b43f296baf99904ee3c
antonin-lfv <antoninlefevre45@icloud.com>
2022-01-09 16:04:34 +0100

    Regressions

--- main.py
+++ main.py
@@ -763,7 +763,7 @@ elif choix_page == "Machine Learning":
                     with exp2:
                         st.warning('Le dataset composé des 2 colonnes selectionnées après dropna() est vide !')
 
-                else:  # Le dataset est viable
+                elif len(st.session_state.choix_features_reg)>1:  # Le dataset est viable
                     df_sans_NaN
                     try:
                         # Data
