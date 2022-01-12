c60087f4a24ff2fc4347e57fe789f3e76d87d031
antonin-lfv <antoninlefevre45@icloud.com>
2022-01-12 21:24:55 +0100

    structure

--- main.py
+++ main.py
@@ -765,8 +765,8 @@ elif choix_page == "Régressions":
                 try:
                     # Data
                     X_train, X_test, y_train, y_test = train_test_split(
-                        st.session_state.data[st.session_state.choix_features_reg].values.reshape(-1, 1),
-                        st.session_state.data[st.session_state.choix_target_reg], test_size=0.4, random_state=4)
+                        df_sans_NaN[st.session_state.choix_features_reg].values.reshape(-1, 1),
+                        df_sans_NaN[st.session_state.choix_target_reg], test_size=0.4, random_state=4)
                     # ###############################################################################
                     with box1_title:
                         st.write("##")
