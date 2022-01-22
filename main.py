a42854dbe4d1a6a5d5a1159d5a6bbfbcb45709d4
antonin-lfv <antoninlefevre45@icloud.com>
2022-01-22 12:16:36 +0100

    Structure

--- main.py
+++ main.py
@@ -34,6 +34,47 @@ st.set_page_config(layout="wide", page_title="No code AI", menu_items={
     'About': "No-code AI Platform - réalisé par Antonin"
 })
 
+st.markdown("""
+<style>
+.first_titre {
+    font-size:75px !important;
+    font-weight: bold;
+    box-sizing: border-box;
+    text-align: center;
+    width: 100%;
+}
+.intro{
+    text-align: justify;
+    font-size:20px !important;
+}
+.grand_titre {
+    font-size:30px !important;
+    font-weight: bold;
+    text-align: center;
+    text-decoration: underline;
+    text-decoration-color: #4976E4;
+    text-decoration-thickness: 5px;
+}
+.section{
+    font-size:20px !important;
+    font-weight: bold;
+    text-align: center;
+    text-decoration: underline;
+    text-decoration-color: #111111;
+    text-decoration-thickness: 3px;
+}
+.petite_section{
+    font-size:16px !important;
+    font-weight: bold;
+}
+.nom_colonne_page3{
+    font-size:17px !important;
+    text-decoration: underline;
+    text-decoration-color: #000;
+    text-decoration-thickness: 1px;
+}
+</style>
+""", unsafe_allow_html=True)
 st.sidebar.image("logo/NAP_logo.png", use_column_width=True)
 st.sidebar.write("##")
 
