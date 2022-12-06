d9ea821003553cd72ace081736375dce320d3804
antoninlefevre <antoninlefevre45@icloud.com>
2022-12-06 23:36:48 +0100

    data from pydataset

--- utils.py
+++ utils.py
@@ -40,8 +40,8 @@ ENSEMBLE_LEARNING_NB_ESTIMATORS = 50
 
 LOCAL_DATASET_NAMES = ["Iris (Classification)", "Penguins (Classification)", "Prix des voitures (Régression)"]
 LOCAL_PATH_DATASET = ['Datasets/iris.csv', 'Datasets/penguins.csv', 'Datasets/CarPrice.csv']
-PYDATASET_NAMES = ["Breakage Angle of Chocolate Cakes"]
-PYDATASET_DISPLAY_NAMES = ["Breakage Angle of Chocolate Cakes"]
+PYDATASET_NAMES = ["cake"]
+PYDATASET_DISPLAY_NAMES = ["cake"]
 
 CSS = """
 <style>
